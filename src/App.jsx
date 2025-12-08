a521186ad6bb568d1e10bfb9e39943384c360d20
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-12-08 14:38:33 +0300

    Integration NoteForm & NoteCard in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState, useEffect, useMemo, useCallback } from "react";
 import NoteForm from "./components/NoteForm";
+import EmptyState from "./components/EmptyState";
+import NoteCard from "./components/NoteCard";
 
 const DEFAULT_CATEGORIES = [
   {
@@ -39,6 +41,69 @@ function App() {
   const [editingNote, setEditingNote] = useState(null);
   const [selectedNote, setSelectedNote] = useState(null);
   const [noteToDelete, setNoteToDelete] = useState(null);
+
+  function handleAddNote(newNote) {
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setIsFormOpen(false);
+  }
+
+  function handleUpdateNote(updatedNote) {
+    setNotes((prev) =>
+      prev.map((note) => (note.id === updatedNote.id ? { ...updatedNote, updatedAt: new Date().toISOString() } : note))
+    );
+
+    setIsFormOpen(false);
+    setEditingNote(null);
+  }
+
+  function handleEditNote(note) {
+    setEditingNote(note);
+    setIsFormOpen(true);
+  }
+
+  function handleDeleteNote(noteId) {
+    setNoteToDelete(noteId);
+    //откроется диалог подвеждения
+  }
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>Notes App</h1>
+        <button onClick={() => setIsFormOpen(true)}>Новая заметка</button>
+      </div>
+      {isFormOpen ? (
+        <NoteForm
+          categories={categories}
+          editingNote={editingNote}
+          onAddNote={handleAddNote}
+          onUpdateNote={handleUpdateNote}
+          onClose={() => {
+            setIsFormOpen(false);
+            setEditingNote(null);
+          }}
+        />
+      ) : (
+        ""
+      )}
+      {notes.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <div className="notes-grid">
+          {notes.map((note) => (
+            <NoteCard
+              key={note.id}
+              note={note}
+              category={categories.find((c) => c.id === note.categoryId)}
+              onNoteClick={() => setSelectedNote(note)}
+              onEdit={() => handleEditNote(note)}
+              onDelete={() => handleDeleteNote(note.id)}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/EmptyState.jsx
@@ -0,0 +1,3 @@
+export default function EmptyState() {
+  return <p>Нет заметок. Создайте первую!</p>;
+}
